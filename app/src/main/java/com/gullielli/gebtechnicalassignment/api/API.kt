c3aa845f021e6437345e149c4235c2a4362f8639
Gudlaugur Ellert <geb@geb.is>
2020-10-15 09:58:14 +1000

    added dependencies retrofit, gson, coroutines. Upversioned some of other dependencies. Added a basic api interface

--- app/src/main/java/com/gullielli/gebtechnicalassignment/api/API.kt
+++ app/src/main/java/com/gullielli/gebtechnicalassignment/api/API.kt
@@ -1,4 +1,16 @@
 package com.gullielli.gebtechnicalassignment.api
 
+import com.gullielli.gebtechnicalassignment.model.QuoteResponse
+import com.gullielli.gebtechnicalassignment.util.Constants.Companion.API_KEY
+import com.gullielli.gebtechnicalassignment.util.Constants.Companion.API_KEY_VALUE
+import com.gullielli.gebtechnicalassignment.util.Constants.Companion.QOD_CATEGORY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
 interface API {
+    @Headers("$API_KEY:$API_KEY_VALUE")
+    @GET(QOD_CATEGORY)
+    suspend fun getQuotes(): Response<QuoteResponse>
 }
