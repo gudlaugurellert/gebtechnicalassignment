ea95f2417ab2abfeb0c1d06009faf3c8cc2bc5f1
Gudlaugur Ellert <geb@geb.is>
2020-10-15 09:52:33 +1000

    added constants that I know will be needed for the api call. added the data classes for the api response

--- app/src/main/java/com/gullielli/gebtechnicalassignment/model/QuoteResponse.kt
+++ app/src/main/java/com/gullielli/gebtechnicalassignment/model/QuoteResponse.kt
@@ -1,2 +1,25 @@
 package com.gullielli.gebtechnicalassignment.model
 
+// Quotes
+data class QuoteResponse(
+
+    val contents: Contents,
+    val success: Success
+)
+
+data class Success(
+    val total: Int
+)
+
+data class Contents(
+    val copyright: String,
+    val quotes: List<Quote>
+)
+
+// the quote response includes a lot more information than what's provided below
+// only using what I need for this assignment
+data class Quote(
+    val author: String,
+    val quote: String,
+    val title: String
+)
--- app/src/main/java/com/gullielli/gebtechnicalassignment/util/Constants.kt
+++ app/src/main/java/com/gullielli/gebtechnicalassignment/util/Constants.kt
@@ -1,4 +1,10 @@
 package com.gullielli.gebtechnicalassignment.util
 
 class Constants {
+    companion object {
+        const val BASE_URL = "https://quotes.rest/"
+        const val QOD_CATEGORY = "/qod?category"
+        const val API_KEY = "X-Theysaidso-Api-Secret"
+        const val API_KEY_VALUE = "DQAkD6YvBozZZm_jf_WQtAeF"
+    }
 }
