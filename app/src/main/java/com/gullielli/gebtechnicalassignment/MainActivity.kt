87245c28f46b0160bd117c2ea5a4cec0519e2ed8
Gudlaugur Ellert <geb@geb.is>
2020-10-15 17:29:33 +1000

    added basic email text validator. added email alert dialog. added email intent to open the phones' email client

--- app/src/main/java/com/gullielli/gebtechnicalassignment/MainActivity.kt
+++ app/src/main/java/com/gullielli/gebtechnicalassignment/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.gullielli.gebtechnicalassignment
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.gullielli.gebtechnicalassignment.model.QuoteResponse
 import com.gullielli.gebtechnicalassignment.repo.Repository
 import com.gullielli.gebtechnicalassignment.util.CheckNetwork
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.alert_prompt.view.*
 import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
 
     // TODO CREATE BASE ACTIVITY
 
     private lateinit var viewModel: MainViewModel
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 
     // Quote of the Day categories
     // TODO IF TIME, MAKE IT NOT HARDCODED
@@ -30,9 +40,10 @@ class MainActivity : AppCompatActivity() {
         authorTV.text = ""
 
         val doWeHaveInternetAccess = CheckNetwork()
+        var internetCheck: Boolean
+
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
-        var internetCheck: Boolean
 
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
 
@@ -42,7 +53,7 @@ class MainActivity : AppCompatActivity() {
 
             if(!internetCheck) {
                 noInternetAccess()
-            } else {
+            } else  {
                 getNewRandomQuote(categories.random())
             }
         }
@@ -51,6 +62,15 @@ class MainActivity : AppCompatActivity() {
             // do something
             // call the phone's mail client and send email there?
             // ...or send email from within the app... decisions decisions...
+            internetCheck = doWeHaveInternetAccess.isNetworkAvail(this)
+
+            if(!internetCheck) {
+                noInternetAccess()
+            } else if (authorTV.equals("")) {
+                failedResponse()
+            } else {
+                addEmailAddresses()
+            }
         }
 
         viewModel.randomQuoteResponse.observe(this, Observer { response ->
@@ -87,5 +107,83 @@ class MainActivity : AppCompatActivity() {
 
     private fun failedResponse() {
         quoteTV.text = getString(R.string.something_went_wrong)
+    }
+
+    private fun addEmailAddresses()  {
+        var email = ""
+
+        //Inflate the dialog with custom alert prompt
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.alert_prompt, null)
+        mDialogView.emailAddressToSend.text = ""
+
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Add email")
+
+        //show dialog
+        val mAlertDialog = mBuilder.show()
+
+        mDialogView.dialogAddEmailBtn.setOnClickListener {
+
+            // Super basic validation to check if there are any illegal characters in the email address
+            if (mDialogView.dialogEmailET.text.toString().matches(emailPattern.toRegex())) {
+
+                email += mDialogView.dialogEmailET.text.toString()+"\n"
+                mDialogView.emailAddressToSend.text = email
+
+                mDialogView.dialogEmailET.text.clear()
+                // Toast - Don't need to show this message - it's annoying from a user point of view
+//                val t = Toast.makeText(this, "Valid email address", Toast.LENGTH_LONG)
+//                t.setGravity(Gravity.CENTER, 0, 0)
+//                t.show()
+
+            } else {
+
+                val t = Toast.makeText(this, "Invalid email address", Toast.LENGTH_LONG)
+                t.setGravity(Gravity.CENTER, 0, 0)
+                t.show()
+            }
+        }
+
+        // Done button
+        mDialogView.dialogDoneBtn.setOnClickListener {
+
+            // If there are no email addresses, then do nothing and display a toast..
+            // If there are email address, then open email client and pass the email(s), quote title, quote text and quote author.
+            if(!email.equals("")) {
+                openEmailClient(email, titleTV.text as String, quoteTV.text as String, authorTV.text as String)
+            } else {
+                val t = Toast.makeText(this, "Don't forget to enter an email address", Toast.LENGTH_LONG)
+                t.setGravity(Gravity.CENTER, 0, 0)
+                t.show()
+            }
+        }
+
+        // Cancel button - dismiss the dialog
+        mDialogView.dialogCancelBtn.setOnClickListener {
+            mAlertDialog.dismiss()
+        }
+    }
+
+    // Not the most elegant way to do this but running out of time.
+    private fun openEmailClient(emailAddresses: String, emailSubject: String, emailQuote: String, emailQuoteAuthor: String) {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto: Example@gmail.com")
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_EMAIL, "test")
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hello World")
+        intent.putExtra(Intent.EXTRA_TEXT, "text body")
+
+        startActivity(intent)
+
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+            //Log.d("Email error:", e.toString())
+            val t = Toast.makeText(this, "Something went wrong opening your email client..", Toast.LENGTH_LONG)
+            t.setGravity(Gravity.CENTER, 0, 0)
+            t.show()
+        }
     }
 }
